c112d910fbbaf1044f849fe2f10d09cc334ad479
Maciej Zurad <maciej.zurad@gmail.com>
2017-07-03 14:28:31 +0200

    removing hash-bang from tf_extractor, adding help to main bash script

--- sync_position_with_image.sh
+++ sync_position_with_image.sh
@@ -1,20 +1,41 @@
 #!/bin/bash
 
-MAP_FRAME_ID=/map
-CAMERA_FRAME_ID=/DJI/gimbal_base/camera
+MAP_FRAME_ID=map
+CAMERA_FRAME_ID=DJI/gimbal_base/camera
+IMAGE_TOPIC=/dji_sdk/image_raw
+
+ROSBAG_RATE=2
+
 ROSBAG=$1
 OUTPUT=$2
+
+PROGRAMNAME=$0
+
+function usage {
+    echo "usage: $PROGRAMNAME <rosbag-file> <output-dir>"
+    echo
+    echo "   <rosbag-file> Path to a ROSBAG file containing TF and image topics"
+    echo "   <output-dir>  Path to an output directory"
+    echo
+    exit 1
+}
+
+if [ "$#" -ne 2 ]; then usage; fi
+if ! [ -e "$1" ]; then echo -e "Rosbag file does not exist!\n"; usage; fi
+
+echo "Extracting transform from ${MAP_FRAME_ID} to ${CAMERA_FRAME_ID}"
+echo "Syncing the transform with ${IMAGE_TOPIC}"
 
 ALL_TRANSFORMS_DIR=${OUTPUT}/numpy
 mkdir -p ${ALL_TRANSFORMS_DIR}
 
 NUMPY_FILE=${ALL_TRANSFORMS_DIR}/poses.npy
 
-python tf_extractor.py -f {MAP_FRAME_ID} -t ${CAMERA_FRAME_ID} -v -o ${NUMPY_FILE} &
+python tf_extractor.py -f ${MAP_FRAME_ID} -t ${CAMERA_FRAME_ID} -v -o ${NUMPY_FILE} &
 echo 'TF extraction node started!'
-rosbag play $ROSBAG
+rosbag play -d 3 -r ${ROSBAG_RATE} $ROSBAG
 echo 'Finished extracting TF transforms'
 kill -INT %1
 echo 'Starting image extract and sync'
-python extract_and_sync.py -b ${ROSBAG} -a ${NUMPY_FILE} ${OUTPUT}
+python extract_and_sync.py -b ${ROSBAG} -a ${NUMPY_FILE} -t ${IMAGE_TOPIC} ${OUTPUT}
 echo 'Finished!'
--- tf_extractor.py
+++ tf_extractor.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python  
 import rospy, tf2_ros, argparse
 import numpy as np
 
